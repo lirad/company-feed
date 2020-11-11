0cb24f9696f20aadecd1a6a510c70305fbf52a49
Diego Lira <diegoalira@gmail.com>
2020-11-11 15:22:34 -0300

    Fix unused component

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,7 +3,6 @@ class ProfilesController < OpinionsController
 
   def show
     @user = User.find(params[:id])
-    @id = params[:id]
     @tweets = @user.opinions.count
     @followers_count = @user.followers.count
     @followers = @user.followers.where.not(id: current_user.id).includes([Photo_attachment: [:blob]])
