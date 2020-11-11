2a7bb181dd4466d0468b387ffb17eb0345427d4d
Diego Lira <diegoalira@gmail.com>
2020-11-11 20:53:44 -0300

    Fix for S3 in production v4

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get '/profile/:id', to: 'profiles#show', as: 'profile'
 
   resources :opinions, only: [:show, :create, :index]
-  resources :followings, only: [:create, :destroy, :index] 
+  resources :followings, only: [:create, :destroy, :index, :show] 
 
   devise_for :users, :controllers => {registrations: 'registrations'}
 
