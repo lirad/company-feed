ce85653411a268665063b2158f4c7a3e3d911e4a
Diego Lira <diegoalira@gmail.com>
2020-11-11 19:53:36 -0300

    Fix Rspec errors

--- spec/models/following_spec.rb
+++ spec/models/following_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Following, type: :model do
   before :each do
-    @user_a = User.create!(FullName: 'Diego Lira', email: 'd@gd.com', password: '123123')
-    @user_b = User.create!(FullName: 'Laura Lira', email: 'l@l.com', password: '123123')
+    @user_a = User.create!(FullName: 'Diego Lira', Username: 'lirad', email: 'd@gd.com', password: '123123')
+    @user_b = User.create!(FullName: 'Laura Lira', Username: 'laub', email: 'l@l.com', password: '123123')
     @a_follows_b = @user_a.following_relationships.create(FollowedId: @user_b.id)
   end
 
--- spec/models/opinion_spec.rb
+++ spec/models/opinion_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Opinion, type: :model do
   before :each do
-    @user_a = User.create(FullName: 'Diego Lira', email: 'd@gd.com', password: '123123')
+    @user_a = User.create(FullName: 'Diego Lira', Username: 'lirad', email: 'd@gd.com', password: '123123')
     @user_a.opinions.build(Text: 'Lorem ipsum')
     @opinion = Opinion.first
   end
