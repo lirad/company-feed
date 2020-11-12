d60d129c1f454b8966ded41ffda35f3ec6907a25
Diego Lira <diegoalira@gmail.com>
2020-11-11 22:35:19 -0300

    Improve unit tests

--- spec/models/following_spec.rb
+++ spec/models/following_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Following, type: :model do
   before :each do
-    @user_a = User.create!(FullName: 'Diego Lira', Username: 'lirad', email: 'd@gd.com', password: '123123')
-    @user_b = User.create!(FullName: 'Laura Lira', Username: 'laub', email: 'l@l.com', password: '123123')
+    @user_a = User.create(FullName: 'Diego Lira', Username: 'lirad', email: 'd@gd.com', password: '123123')
+    @user_b = User.create(FullName: 'Laura Lira', Username: 'laub', email: 'l@l.com', password: '123123')
     @a_follows_b = @user_a.following_relationships.create(FollowedId: @user_b.id)
   end
 
@@ -19,6 +19,14 @@ RSpec.describe Following, type: :model do
     it 'user can unfollow another user' do
       @user_a.following_relationships.find_by(FollowedId: @user_b.id).destroy
       expect(@user_b.followings.first).to eq(nil)
+    end
+
+    it 'user can list followers' do
+      expect(@user_b.followers.length).to eq(1)
+    end
+
+    it 'user can list follwings' do
+      expect(@user_a.followings.length).to eq(1)
     end
   end
 end
